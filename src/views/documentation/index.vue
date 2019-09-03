e7cad5611a4dc8164b3e6338e40a550e084ee8cb
liuxianfeng521 <735801190@qq.com>
2019-09-03 23:32:41 +0800

    子组件打开对话框操作

--- src/views/documentation/index.vue
+++ src/views/documentation/index.vue
@@ -1,10 +1,27 @@
 <template>
-  <div>documentation</div>
+  <div>
+    <el-button @click="dialogVisible = true" >打开对话框</el-button>
+    <edit :visible.sync="dialogVisible" @closeIt="closeIt()" />
+  </div>
 </template>
 
 <script>
+import edit from '../edit.vue'
 export default {
-  name: 'Index'
+  name: 'Index',
+  components: {
+    edit
+  },
+  data() {
+    return {
+      dialogVisible: false
+    }
+  },
+  methods: {
+    closeIt() {
+      this.dialogVisible = false
+    }
+  }
 }
 </script>
 
