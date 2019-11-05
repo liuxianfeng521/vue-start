a2a4a070417506e894c802d91f36856a7a469954
liuxianfeng521 <735801190@qq.com>
2019-11-05 16:59:29 +0800

    添加tab模块和显示错误模块

--- src/router/index.js
+++ src/router/index.js
@@ -220,7 +220,7 @@ export const asyncRoutes = [
       }
     ]
   },
-
+*/
   {
     path: '/tab',
     component: Layout,
@@ -246,19 +246,19 @@ export const asyncRoutes = [
     children: [
       {
         path: '401',
-        component: () => import('@/views/error-page/401'),
+        component: () => import('@/views/errorPage/401'),
         name: 'Page401',
         meta: { title: '401', noCache: true }
       },
       {
         path: '404',
-        component: () => import('@/views/error-page/404'),
+        component: () => import('@/views/errorPage/404'),
         name: 'Page404',
         meta: { title: '404', noCache: true }
       }
     ]
   },
-
+  /*
   {
     path: '/error-log',
     component: Layout,
