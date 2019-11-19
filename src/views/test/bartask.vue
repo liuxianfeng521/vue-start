53963d642b7c6c0f639df7c6fe41f85ac4d24743
yangliwen123 <2782385499@qq.com>
2019-11-19 16:25:46 +0800

    增加barTask柱状图组件

--- src/views/test/bartask.vue
+++ src/views/test/bartask.vue
@@ -1,12 +1,22 @@
 <template>
-  <div>
-    vvvvvvvvvvvvvvvvv
+  <div class="dashboard-editor-container">
+    <el-row :gutter="32">
+      <el-col :xs="24" :sm="24" :lg="8">
+        <div class="chart-wrapper">
+          <bar-chart />
+        </div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
+import BarChart from '@/views/test/echarts/barchart'
 export default {
   name: 'Bartask',
+  components: {
+    BarChart
+  },
   data() {
     return {
 
@@ -15,6 +25,13 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+  .dashboard-editor-container {
+    padding: 20px;
+    position: relative;
+  .chart-wrapper {
+    background: #fff;
+    padding: 20px;
+  }
+  }
 </style>
